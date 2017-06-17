a6214ce80953dabe8e79f3e3f32e61910c69dde1
fingertippower <939408507@qq.com>
2017-06-17 14:27:41 +0800

    项目初步配置

---
+++ src/components/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="app">
+        <p>222233</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'app',
+        data () {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    @import '../assets/css/function.scss';
+</style>
