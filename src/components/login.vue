005af44ab492b2d956ff09b39fa27ed9c4ac9efd
Rainchess <2430153935@qq.com>
2017-07-17 17:28:10 +0800

    登录界面

--- src/components/login.vue
+++ src/components/login.vue
@@ -9,16 +9,27 @@
             </p>
         </div>
         <div class="down">
-            <span class="span1">
-                <img src="../../img/phone.png">
-            </span>
-            <input class="text1" type="text" placeholder="输入手机号">
-            <input class="text2" type="password" placeholder="输入密码">
+            <div class="phone">
+                <span>
+                    <img src="../../img/phone.png">
+                </span>
+                <input class="text1" type="text" placeholder="输入手机号">
+            </div>
+            <div class="password">
+                <span>
+                    <img src="../../img/password.png">
+                </span>
+                <input class="text2" type="password" placeholder="输入密码">
+            </div>
             <button class="btn1">立即注册</button>
             <button class="btn2">忘记密码</button>
             <button class="btn3">登录</button>
             <hr>
-            <span class="span2">快速登录</span>
+            <span class="span3">快速登录</span>
+            <div class="div1">
+                <img src="../../img/QQ.png">
+                <img src="../../img/weixin.png">
+            </div>
         </div>
     </div>
 </template>
@@ -32,17 +43,19 @@
     *{
         margin: 0;
         padding: 0;
+        background-color: antiquewhite;
     }
     .up{
         width: px2rem(605px);
         height: px2rem(460px);
-        background-color: #fff;
         margin: px2rem(95px) auto;
         margin-bottom: 0;
-        border: px2rem(1px) solid;
+        border: px2rem(1px) solid ;
         border-radius: px2rem(10px) px2rem(10px) 0 0;
         position: relative;
         text-align: center;
+        background-color: #fff;
+        opacity: 0.5;
     }
     .up img{
         width: px2rem(115px);
@@ -61,7 +74,6 @@
         left:0;
         right:0;
         top:px2rem(216.667px);
-        margin:auto;
         position: absolute;
     }
     .up p{
@@ -70,22 +82,27 @@
         left:0;
         right:0;
         top:px2rem(335.7383px);
-        margin:auto;
         position: absolute;
         font-family: 宋体;
     }
     .down{
         width: px2rem(605px);
         height: px2rem(710px);
-        background-color: #fff;
         margin: px2rem(16px) auto;
         border: px2rem(1px) solid;
-        border-radius:  0 0 px2rem(10px) px2rem(10px);
+        border-radius: 0 0 px2rem(10px) px2rem(10px);
         position: relative;
         text-align: center;
     }
-    .down .span1{
-        margin-left: px2rem(27.4912px);
+    .down .phone span{
+        margin-left: px2rem(7px);
+        margin-top: px2rem(98px);
+        position: absolute;
+    }
+    .down .password span{
+        margin-left: px2rem(7px);
+        margin-top: px2rem(46.333px);
+        position: absolute;
     }
     .down input{
         width: px2rem(514.9722px);
@@ -96,19 +113,24 @@
         border-left-style: none;
         border-right-style: none;
         border-color: #26A8F7;
-        color: #26A8F7;
+        color: #38B0F9;
         font-size: px2rem(30px);
         padding-left: px2rem(34px);
+
     }
     input::-webkit-input-placeholder{
-        color: #26A8F7;
+        color: #38B0F9;
     }
     .down input.text1{
         margin-top: px2rem(95px);
     }
-    .down img{
+    .down .phone img{
         width: px2rem(20.792px);
         height: px2rem(34px);
+    }
+    .down .password img{
+        width: px2rem(20.792px);
+        height: px2rem(28.667px);
     }
     .down button{
         font-size: px2rem(18px);
@@ -118,7 +140,7 @@
         border: none;
     }
     .down button.btn1{
-        color: #26A8F7;
+        color: #38B0F9;
         float: left;
         margin-left: px2rem(66.6616px);
     }
@@ -132,19 +154,19 @@
         color: #ffffff;
         width: px2rem(415.3335px);
         height: px2rem(82.124px);
-        margin-top: px2rem(95.792px);
-        background-color: #26A8F7;
+        margin-top: px2rem(95px);
+        background-color: #38B0F9;
         border-radius: px2rem(10px) px2rem(10px) px2rem(10px) px2rem(10px);
     }
     .down hr{
         width: px2rem(548.9722px);
-        margin: px2rem(146.9922px) auto;
+        margin: px2rem(132.084px) auto;
         margin-bottom: 0;
         color: #A5A5A5;
         text-align: center;
         position: relative;
     }
-    .down .span2{
+    .down .span3{
         width: px2rem(130px);
         background-color: #ffffff;
         font-size: px2rem(24px);
@@ -152,5 +174,12 @@
         margin:px2rem(-20px) px2rem(-65px);
         position: absolute;
         text-align:center;
+        background-color: transparent;
+    }
+    .down .div1 img{
+        width: px2rem(70px);
+        height: px2rem(70px);
+        margin:px2rem(32.499px) px2rem(31px);
+        margin-bottom: 0;
     }
 </style>
