54645537355774eba3dc3f7b33eb9d4a82a265c3
Rainchess <2430153935@qq.com>
2017-07-18 17:14:41 +0800

    登录界面

--- src/components/login.vue
+++ src/components/login.vue
@@ -10,25 +10,34 @@
         </div>
         <div class="down">
             <div class="phone">
-                <span>
-                    <img src="../../img/phone.png">
+                <span class="span1">
+                    <img class="img1" src="../../img/phone.png">
                 </span>
                 <input class="text1" type="text" placeholder="输入手机号">
+                <span class="span2">
+                    <img class="img2" src="../../img/delete.png">
+                </span>
             </div>
             <div class="password">
-                <span>
-                    <img src="../../img/password.png">
+                <span class="span4">
+                    <img class="img3" src="../../img/password.png">
                 </span>
                 <input class="text2" type="password" placeholder="输入密码">
+                <span class="span2">
+                    <img class="img2" src="../../img/delete.png">
+                </span>
             </div>
             <button class="btn1">立即注册</button>
             <button class="btn2">忘记密码</button>
             <button class="btn3">登录</button>
-            <hr>
-            <span class="span3">快速登录</span>
+            <div class="div2">
+                <hr class="hr1">
+                <hr class="hr2">
+                <span class="span3">快速登录</span>
+            </div>
             <div class="div1">
-                <img src="../../img/QQ.png">
-                <img src="../../img/weixin.png">
+                <img class="QQ" src="../../img/QQ.png">
+                <img class="weixin" src="../../img/weixin.png">
             </div>
         </div>
     </div>
@@ -43,10 +52,8 @@
     *{
         margin: 0;
         padding: 0;
-        background-color: tan;
     }
     .background{
-        background-color: tan;
         width: 100%;
         height: 100%;
         margin: 0;
@@ -115,13 +122,25 @@
     .down .password{
         background-color: transparent;
     }
-    .down .phone span{
+    .down .phone .span1{
         margin-left: px2rem(7px);
         margin-top: px2rem(98px);
         position: absolute;
         background-color: transparent;
     }
-    .down .password span{
+    .down .phone .span2{
+        margin-left: px2rem(-36.4063px);
+        margin-top: px2rem(107.8018px);
+        position: absolute;
+        background-color: transparent;
+    }
+    .down .password .span2{
+        margin-left: px2rem(-36.4063px);
+        margin-top: px2rem(51.8818px);
+        position: absolute;
+        background-color: transparent;
+    }
+    .down .password .span4{
         margin-left: px2rem(7px);
         margin-top: px2rem(46.333px);
         position: absolute;
@@ -147,12 +166,17 @@
     .down input.text1{
         margin-top: px2rem(95px);
     }
-    .down .phone img{
+    .down .phone .img1{
         width: px2rem(20.792px);
         height: px2rem(34px);
         background-color: transparent;
     }
-    .down .password img{
+    .down .img2{
+        width: px2rem(22.4063px);
+        height: px2rem(22.4063px);
+        background-color: transparent;
+    }
+    .down .password .img3{
         width: px2rem(20.792px);
         height: px2rem(28.667px);
         background-color: transparent;
@@ -183,20 +207,27 @@
         background-color: #38B0F9;
         border-radius: px2rem(10px) px2rem(10px) px2rem(10px) px2rem(10px);
     }
-    .down hr{
-        width: px2rem(548.9722px);
+    .down .div2 hr{
+        width: px2rem(208px);
         margin: px2rem(132.084px) auto;
         margin-bottom: 0;
         color: #A5A5A5;
-        text-align: center;
         position: relative;
+    }
+    .down .div2 .hr1{
+        float: left;
+        left: px2rem(29.1577px);
+    }
+    .down .div2 .hr2{
+        float: right;
+        right: px2rem(29.1577px);
     }
     .down .span3{
         width: px2rem(130px);
         background-color: #ffffff;
         font-size: px2rem(24px);
         color: #A5A5A5;
-        margin:px2rem(-20px) px2rem(-65px);
+        margin:px2rem(119px) px2rem(-65px);
         position: absolute;
         text-align:center;
         background-color: transparent;
@@ -207,8 +238,14 @@
     .down .div1 img{
         width: px2rem(70px);
         height: px2rem(70px);
-        margin:px2rem(32.499px) px2rem(31px);
+        margin-top: px2rem(172.084px);
         margin-bottom: 0;
         background-color: transparent;
+    }
+    .down .div1 .QQ{
+        float: left;
+    }
+    .down .div1 .weixin{
+        float: right;
     }
 </style>
