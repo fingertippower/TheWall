b8cdef0c9d0969e55acab4a59ac1d953c6eb31a8
Rainchess <2430153935@qq.com>
2017-07-18 17:15:16 +0800

    注册界面

--- src/components/register.vue
+++ src/components/register.vue
@@ -20,19 +20,25 @@
             </div>
             <div class="password">
                 <span class="span1">
-                    <img src="../../img/password.png">
+                    <img class="img1" src="../../img/password.png">
                 </span>
                 <input class="text2" type="password" placeholder="输入密码">
+                <span class="span2">
+                    <img class="img2" src="../../img/delete.png">
+                </span>
             </div>
             <button class="btn1">立即注册</button>
             <button class="btn2">忘记密码</button>
             <input class="text3" type="text" placeholder="输入验证码">
             <button class="btn3">注册</button>
-            <hr>
-            <span class="span3">快速登录</span>
+            <div class="div2">
+                <hr class="hr1">
+                <hr class="hr2">
+                <span class="span3">快速登录</span>
+            </div>
             <div class="div1">
-                <img src="../../img/QQ.png">
-                <img src="../../img/weixin.png">
+                <img class="QQ" src="../../img/QQ.png">
+                <img class="weixin" src="../../img/weixin.png">
             </div>
         </div>
     </div>
@@ -47,10 +53,8 @@
     *{
         margin: 0;
         padding: 0;
-        background-color: tan;
     }
     .background{
-        background-color: tan;
         width: 100%;
         height: 100%;
         margin: 0;
@@ -145,9 +149,14 @@
         height: px2rem(34px);
         background-color: transparent;
     }
-    .down .password img{
+    .down .img1{
         width: px2rem(20.792px);
         height: px2rem(28.667px);
+        background-color: transparent;
+    }
+    .down .img2{
+        width: px2rem(22.4063px);
+        height: px2rem(22.4063px);
         background-color: transparent;
     }
     .down input.text1{
@@ -184,6 +193,12 @@
     .down .phone .span2{
         margin-left: px2rem(-116.667px);
         margin-top: px2rem(46.333px);
+        position: absolute;
+        background-color: transparent;
+    }
+    .down .password .span2{
+        margin-left: px2rem(-36.4063px);
+        margin-top: px2rem(51.8818px);
         position: absolute;
         background-color: transparent;
     }
@@ -230,22 +245,30 @@
         background-color: #38B0F9;
         border-radius: px2rem(10px) px2rem(10px) px2rem(10px) px2rem(10px);
     }
-    .down hr{
-        width: px2rem(548.9722px);
-        margin: px2rem(120.4504px) auto;
+    .down .div2 hr{
+        width: px2rem(208px);
+        margin: px2rem(121.5762px) auto;
         margin-bottom: 0;
         color: #A5A5A5;
-        text-align: center;
         position: relative;
+    }
+    .down .div2 .hr1{
+        float: left;
+        left: px2rem(29.1577px);
+    }
+    .down .div2 .hr2{
+        float: right;
+        right: px2rem(29.1577px);
     }
     .down .span3{
         width: px2rem(130px);
         background-color: #ffffff;
         font-size: px2rem(24px);
         color: #A5A5A5;
-        margin:px2rem(-20px) px2rem(-65px);
+        margin:px2rem(107px) px2rem(-65px);
         position: absolute;
         text-align:center;
+        background-color: transparent;
     }
     .down .div1{
         background-color: transparent;
@@ -253,8 +276,14 @@
     .down .div1 img{
         width: px2rem(70px);
         height: px2rem(70px);
-        margin:px2rem(32.499px) px2rem(31px);
+        margin-top: px2rem(158.5762px);
         margin-bottom: 0;
         background-color: transparent;
+    }
+    .down .div1 .QQ{
+        float: left;
+    }
+    .down .div1 .weixin{
+        float: right;
     }
 </style>
