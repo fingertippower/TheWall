fb798070c98393492152c3b8ba220b70802fc5db
Rainchess <2430153935@qq.com>
2017-08-15 18:50:54 +0800

    收藏页面

--- src/components/collect.vue
+++ src/components/collect.vue
@@ -1,8 +1,12 @@
 <template>
-    <div>
+    <div class="background">
         <div class="head">
             <img src="../assets/img/return.png">
             <p>收藏</p>
+        </div>
+        <div class="collect">
+            <p>五点二十分中央大街</p>
+            <img src="../assets/img/background.png">
         </div>
     </div>
 </template>
@@ -13,6 +17,12 @@
 
 <style lang="scss" scoped>
     @import '../assets/css/function.scss';
+    .background{
+        width: 100%;
+        height: 100%;
+        background-color: #F6EDDC;
+        position: fixed;
+    }
     .head{
         width:100%;
         height:px2rem(109.1172px);
@@ -33,5 +43,27 @@
         right:0;
         top:px2rem(55.4971px);
         position: absolute;
+    }
+    .collect{
+        width: px2rem(691px);
+        height: px2rem(282.8843px);
+        margin: px2rem(6px) auto;
+        margin-bottom: px2rem(340px);
+        border-radius: px2rem(10px) px2rem(10px) px2rem(10px) px2rem(10px);
+        background-color: transparent;
+        background-color: rgba(255,255,255,0.5);
+        box-shadow:0 0 px2rem(4px) dimgrey;
+    }
+    .collect p{
+        font-size: px2rem(26px);
+        margin-top: px2rem(107px);
+        margin-left: px2rem(25px);
+        position: absolute;
+    }
+    .collect img{
+        width: px2rem(194px);
+        height: px2rem(282.8843px);
+        float: right;
+        border-radius: 0 px2rem(10px) px2rem(10px) 0;
     }
 </style>
