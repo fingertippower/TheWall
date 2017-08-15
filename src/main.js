99ee55d2ddc44f4ed21aa3397b56075f991ac781
Lcj-space <1367546524@qq.com>
2017-08-15 23:10:58 +0800

    引入了饿了么UI，便于快速建站

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './app.vue'
 import router from './router'
+//引入饿了么UI
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+Vue.use(ElementUI);
 new Vue({
     el: '#app',
     router,
