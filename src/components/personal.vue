7c29cfd9ff2c13bf93c4be20033e1d4f897817aa
Rainchess <2430153935@qq.com>
2017-08-16 20:58:01 +0800

    个人信息页面

--- src/components/personal.vue
+++ src/components/personal.vue
@@ -5,8 +5,16 @@
             回忆专用小马甲
             <img class="woman" src="../assets/img/woman.png">
         </p>
-        <hr/>
-        <img class="collection" src="../assets/img/collection.png">
+        <hr class="long-hr">
+        <img class="public" id="collection" src="../assets/img/collection.png">
+        <span>我的收藏</span>
+        <hr class="short-hr">
+        <img class="public" id="school" src="../assets/img/school.png">
+        <span>学校信息</span>
+        <hr class="short-hr">
+        <img class="public" id="name" src="../assets/img/name.png">
+        <span>真实姓名</span>
+        <hr class="short-hr">
     </div>
 </template>
 
@@ -44,13 +52,32 @@
         width: px2rem(21px);
         height: px2rem(24.237px);
     }
-    hr{
+    .long-hr{
         margin-top: px2rem(35px);
     }
-    .collection{
+    .public{
         width: px2rem(32px);
         height: px2rem(24px);
         margin-left: px2rem(105px);
+        margin-top: px2rem(29.2397px);
+    }
+    #collection{
         margin-top: px2rem(123px);
     }
+    #name{
+        width: px2rem(21px);
+        height: px2rem(32px);
+        margin-left: px2rem(110.5px);
+    }
+    span{
+        color: #8E8D8D;
+        font-size: px2rem(30px);
+        margin-left: px2rem(56.0869px);
+    }
+    .short-hr{
+        width: px2rem(314.9844px);
+        margin-left: px2rem(100px);
+        margin-top: px2rem(29.2397px);
+    }
+
 </style>
