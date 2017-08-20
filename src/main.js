98ee471970ccfa713d2ec5ccfcad5f16209a6ad7
fingertippower <939408507@qq.com>
2017-08-20 21:15:46 +0800

    项目使用vuex管理数据，每个页面对应的在modules中新建相应页面的状态管理

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './app.vue'
 import router from './router'
+import store from './vuex/store'
 //引入饿了么UI
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
@@ -8,5 +9,6 @@ Vue.use(ElementUI);
 new Vue({
     el: '#app',
     router,
+    store,
     render: h => h(App)
 })
