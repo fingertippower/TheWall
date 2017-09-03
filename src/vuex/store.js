ad1f4d011e115a6953af58e100a88f0c8f3de7d0
fingertippower <939408507@qq.com>
2017-09-03 18:55:48 +0800

    发送信件页面主体一个完成，差页面在刚刚创建的时候时间轴颜色和信件的默认设置，明天完成

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import IndexFoot from './modules/indexFoot'
 import Concern from './modules/concern'
+import SendLetter from './modules/send-letter'
 
 Vue.use(Vuex);
 //这里并没有把所有的vuex里面的内容写在一起，因为那样显得很乱，而是把不同页面的放在了一起最后暴露出去
 export default new Vuex.Store({
     modules:{
         IndexFoot,
-        Concern
+        Concern,
+        SendLetter
     }
 })
