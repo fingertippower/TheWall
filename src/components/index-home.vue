5e5c5eb8de2622fd6b1f8fedd06dfff30edcd82f
fingertippower <939408507@qq.com>
2017-09-03 19:16:53 +0800

    发送信件页面主体一个完成，差页面在刚刚创建的时候时间轴颜色和信件的默认设置，明天完成

---
+++ src/components/index-home.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="index-home">
+        <v-headnotice></v-headnotice>
+        <v-headnavigation></v-headnavigation>
+        <v-list></v-list>
+        <router-view></router-view>
+        <v-pen></v-pen>
+        <v-foot></v-foot>
+    </div>
+</template>
+
+<script>
+    import headnotice from './headnotice.vue'
+    import headnavigation from './headnavigation.vue'
+    import foot from './foot.vue'
+    import indexList from './index-list.vue'
+    import indexPen from './index-pen.vue'
+    export default {
+        name: 'app',
+        data () {
+            return {
+
+            }
+        },
+        components:{
+            'v-headnotice':headnotice,
+            'v-headnavigation':headnavigation,
+            'v-foot':foot,
+            'v-list':indexList,
+            'v-pen':indexPen
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import '../assets/css/function.scss';
+    #index-home{
+        position: absolute;
+        top: 0;
+        width: 100%;
+        background-image: url("../assets/img/indexBackground2.png");
+        background-size: 100% 100%;
+        background-attachment:fixed;
+    }
+</style>
---
+++ src/components/letter-head.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="letter">
+        <div class="letter-head"></div>
+        <div class="letter-body">
+            <div class="getOrPut">
+
+                    <div class="getLetter">
+                        收件箱
+                    </div>
+
+                <router-link to="/index/letter/letter-put">
+                    <div class="putLetter">
+                        已发送
+                    </div>
+                </router-link>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+    @import '../assets/css/function.scss';
+    .letter-head{
+        width: 100%;
+        height: px2rem(107px);
+        background-color: rgba(244,173,18,1);
+    }
+    .letter-body{
+        width: 100%;
+        .getOrPut{
+            width: 100%;
+            height: px2rem(105px);
+            .getLetter{
+                width: px2rem(189px);
+                height: px2rem(53px);
+                border: 1px solid rgba(234,76,137,1);
+                color: rgba(234,76,137,1);
+                text-align: center;
+                font-size: px2rem(40px);
+                border-radius: px2rem(10px);
+                margin-top: px2rem(23px);
+                float: left;
+                margin-left: px2rem(49px);
+            }
+            .putLetter{
+                width: px2rem(189px);
+                height: px2rem(53px);
+                border: 1px solid rgba(234,76,137,1);
+                color: rgba(234,76,137,1);
+                text-align: center;
+                font-size: px2rem(40px);
+                border-radius: px2rem(10px);
+                margin-top: px2rem(23px);
+                margin-right: px2rem(49px);
+                float: right;
+            }
+        }
+    }
+</style>
