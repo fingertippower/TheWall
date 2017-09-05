a0bfb7cfc69da67bf280ed877fc293f3ff9795cc
fingertippower <939408507@qq.com>
2017-09-05 20:28:08 +0800

    解决了一个ES6语法无法正确解析的错误

---
+++ src/vuex/modules/confession-letter-talk.js
@@ -0,0 +1,25 @@
+import * as types from '../types'
+
+const state = {
+
+}
+
+const mutations = {
+
+}
+
+const actions = {
+    aaa:function(){
+        console.log(111)
+    }
+}
+
+const getters = {
+
+}
+export default {
+    state,
+    mutations,
+    actions,
+    getters
+}
