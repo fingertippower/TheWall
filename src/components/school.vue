0432937e75e5e6546a00229b1776ba3d02d80059
Rainchess <2430153935@qq.com>
2017-10-31 13:50:36 +0800

    学校信息页面

--- src/components/school.vue
+++ src/components/school.vue
@@ -6,38 +6,31 @@
         </div>
         <div class="message">
             <span>学校</span>
-            <input type="text">
+            <input type="text" id="school" value="">
             <br/>
             <span>学院</span>
-            <input type="text">
+            <input type="text" id="academy" value="">
             <br/>
             <span>专业</span>
-            <input type="text">
+            <input type="text" id="major" value="">
             <br/>
             <span>年级</span>
-            <input type="text">
+            <input type="text" id="grade" value="">
             <br/>
             <span>班级</span>
-            <input type="text">
+            <input type="text" id="class" value="">
         </div>
         <div class="save">
-            <button>保存</button>
-        </div>
-        <div class="shave">
-            <hr class="hr1">
-            <hr class="hr2">
-            <span>分享<br/>邀请校友</span>
-        </div>
-        <div class="chat">
-            <img class="QQ" src="../assets/img/QQ.png">
-            <img class="WeChat" src="../assets/img/WeChat.png">
+            <button @click="save()">保存</button>
         </div>
     </div>
 </template>
 
 <script>
+    import {mapActions} from 'vuex'
     export default{
         methods:{
+            ...mapActions(['save']),
             goBack:function () {
                 this.$router.go(-1);
             }
@@ -53,7 +46,7 @@
     }
     .head{
         width:100%;
-        height:px2rem(109.1172px);
+        height:px2rem(110px);
         background-color: #F4AD12;
         position: relative;
     }
@@ -79,74 +72,34 @@
     }
     .message input{
         width: px2rem(550px);
-        height: px2rem(89px);
+        height: px2rem(90px);
         border: px2rem(1px) solid;
         border-top-style: none;
         border-left-style: none;
         border-right-style: none;
         border-color: #B8B8B8;
         color: #B8B8B8;
-        font-size: px2rem(34px);
-        padding-left: px2rem(120px);
+        font-size: px2rem(35px);
+        padding-left: px2rem(130px);
         padding-top: px2rem(30px);
     }
     .message span{
-        font-size: px2rem(34px);
+        font-size: px2rem(35px);
         margin-left: px2rem(31px);
-        margin-top: px2rem(52px);
+        margin-top: px2rem(53px);
         position: absolute;
     }
     .save{
         text-align: center;
     }
     button{
-        width: px2rem(306px);
-        height: px2rem(67.45px);
-        margin-top: px2rem(280.9131px);
+        width: px2rem(310px);
+        height: px2rem(70px);
+        margin-top: px2rem(380px);
         background-color: #F4AD12;
         color: #fff;
         border: none;
         border-radius: px2rem(6px);
         font-size: px2rem(52px);
-    }
-    hr{
-        width: px2rem(296px);
-        margin: px2rem(260px) auto;
-        margin-bottom: 0;
-        color: #A5A5A5;
-        position: relative;
-    }
-    .hr1{
-        float: left;
-        left: px2rem(29.1577px);
-    }
-    .hr2{
-        float: right;
-        right: px2rem(29.1577px);
-    }
-    .shave{
-        text-align: center;
-    }
-    .shave span{
-        width: px2rem(109px);
-        font-size: px2rem(18px);
-        color: #8C8C8B;
-        margin:px2rem(238px) px2rem(-54.5px);
-        margin-bottom: 0;
-        position: absolute;
-        text-align:center;
-        background-color: transparent;
-    }
-    .chat img{
-        width: px2rem(70px);
-        height: px2rem(71px);
-        margin-top:px2rem(330px);
-        position: absolute;
-    }
-    .QQ{
-        margin-left: px2rem(-31px);
-    }
-    .WeChat{
-        margin-left: px2rem(101px);
     }
 </style>
