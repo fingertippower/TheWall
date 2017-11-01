2c15d11f049716e6674a6584eca5a72c7d6d7e0f
fingertippower <939408507@qq.com>
2017-11-01 20:51:05 +0800

    张的小错误

--- src/components/index-list.vue
+++ src/components/index-list.vue
@@ -118,7 +118,7 @@
     }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss" rel="stylesheet/scss" scoped>
     @import '../assets/css/function.scss';
     .indexList{
         margin-top:px2rem(215px);
