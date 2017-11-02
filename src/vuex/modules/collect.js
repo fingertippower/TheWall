8dec2e159076668343031f358daf72f4bd76e48a
Rainchess <2430153935@qq.com>
2017-11-02 12:04:26 +0800

    我的收藏页面

--- src/vuex/modules/collect.js
+++ src/vuex/modules/collect.js
@@ -10,22 +10,6 @@ const state={
             collectText:'修订版市场部被称，为难吃武功长鼻王建安成本卡不与。',
             collectTime:'2017-08-18',
             collectContentImg:"/src/assets/img/background.png"
-        },
-        {
-            collectPeopleImg:'/src/assets/img/people.png',
-            collectNickname:'大长腿',
-            collectTitle:'五点二十分中央大街',
-            collectText:'修订版市场部被称，为难吃武功长鼻王建安成本卡不与。',
-            collectTime:'2017-08-18',
-            collectContentImg:"/src/assets/img/background.png"
-        },
-        {
-            collectPeopleImg:'/src/assets/img/people.png',
-            collectNickname:'大长腿',
-            collectTitle:'五点二十分中央大街',
-            collectText:'修订版市场部被称，为难吃武功长鼻王建安成本卡不与。',
-            collectTime:'2017-08-18',
-            collectContentImg:"/src/assets/img/background.png"
         }
     ]
 };
